d56e8cb5b5b63dc06a215704e0713ea10787109e
mrbrunelli <matheus.brunelli@gmail.com>
2022-05-21 18:02:53 -0300

    feat: add GetItem UseCase

---
+++ src/Application/UseCase/GetItem/GetItem.ts
@@ -0,0 +1,22 @@
+import RepositoryFactory from "../../../Domain/Factory/RepositoryFactory";
+import ItemRepository from "../../../Domain/Repository/ItemRepository";
+import GetItemOutput from "./GetItemOutput";
+
+export default class GetItem {
+    itemRepository: ItemRepository;
+
+    constructor(readonly repositoryFactory: RepositoryFactory) {
+        this.itemRepository = repositoryFactory.createItemRepository();
+    }
+
+    async execute(id: string): Promise<GetItemOutput> {
+        const item = await this.itemRepository.getById(id);
+
+        if (!item) {
+            throw new Error("Item not Found");
+        }
+
+        const output = new GetItemOutput(item._id, item.description, item.price);
+        return output;
+    }
+}
---
+++ src/Application/UseCase/GetItem/GetItemOutput.ts
@@ -0,0 +1,3 @@
+export default class GetItemOutput {
+    constructor(readonly id: string, readonly description: string, readonly price: number) {}
+}
