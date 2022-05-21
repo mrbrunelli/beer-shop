45bf1f3c7013aac757fe3dd0a2b4d79850e0e692
mrbrunelli <matheus.brunelli@gmail.com>
2022-05-21 09:32:27 -0300

    refactor: add price and complete test case

--- src/Domain/Entity/Item.ts
+++ src/Domain/Entity/Item.ts
@@ -4,6 +4,7 @@ export default class Item {
     constructor(
         readonly id: string,
         readonly description: string,
+        readonly price: number,
         readonly size: number,
         readonly abv: number,
         readonly ingredients: Ingredients
--- test/unit/Item.test.ts
+++ test/unit/Item.test.ts
@@ -1,7 +1,12 @@
 import Ingredients from "../../src/Domain/Entity/Ingredients";
 import Item from "../../src/Domain/Entity/Item";
 
-test("Should to be defined", () => {
-    const item = new Item("1", "Spaten", 355, 5.8, new Ingredients("água", "malte", "lúpulo"));
+test("Should to be defined with correctly values", () => {
+    const item = new Item("1", "Spaten", 4.98, 355, 5.8, new Ingredients("água", "malte", "lúpulo"));
     expect(item).toBeDefined();
+    expect(item.description).toBe("Spaten");
+    expect(item.price).toBe(4.98);
+    expect(item.size).toBe(355);
+    expect(item.abv).toBe(5.8);
+    expect(item.ingredients.getIngredients()).toBe("água, malte, lúpulo");
 });
