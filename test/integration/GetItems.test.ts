85c9959b3b9fb56f892a20b5dab4987c73db9c22
Matheus Ricardo Brunelli <matheus.brunelli@gmail.com>
2022-05-24 20:54:40 +0000

    refactor: move GetItems test cases to a separated file

--- test/integration/GetItems.test.ts
+++ test/integration/GetItems.test.ts
@@ -1,4 +1,4 @@
-import GetItem from "../../src/Application/UseCase/GetItem/GetItem";
+import GetItems from "../../src/Application/UseCase/GetItems/GetItems";
 import RegisterItem from "../../src/Application/UseCase/RegisterItem/RegisterItem";
 import Ingredients from "../../src/Domain/Entity/Ingredients";
 import RepositoryFactory from "../../src/Domain/Factory/RepositoryFactory";
@@ -24,28 +24,30 @@ afterEach(async () => {
     await connection.close();
 });
 
-test("Should get Item by id", async () => {
-    const registerItem = new RegisterItem(repositoryFactory, objectIdGenerator);
-    const input = {
-        description: "Ribeirão Lager",
-        price: 8.9,
-        abv: 5.2,
-        size: 600,
-        ingredients: new Ingredients("água", "malte", "lúpulo", "laranja"),
-    };
-    const registeredItem = await registerItem.execute(input);
+test("Should get all Items", async () => {
+  const registerItem = new RegisterItem(repositoryFactory, objectIdGenerator);
 
-    const getItem = new GetItem(repositoryFactory);
-    const output = await getItem.execute(registeredItem.id);
+  Array.from(Array(10)).forEach(async () => {
+      const input = {
+          description: "Ribeirão Lager",
+          price: 8.9,
+          abv: 5.2,
+          size: 600,
+          ingredients: new Ingredients("água", "malte", "lúpulo", "laranja"),
+      };
+      await registerItem.execute(input);
+  });
 
-    expect(output).toBeDefined();
-    expect(output.id).toBe(registeredItem.id);
+  const getItems = new GetItems(repositoryFactory);
+  const output = await getItems.execute();
+
+  expect(output).toBeDefined();
+  expect(output).toHaveLength(10);
 });
 
-test("Should throws exception if Item does not exists", async () => {
-    const getItem = new GetItem(repositoryFactory);
-    const inexistentId = "62895a88fd0d2f46b2d6bdc9";
-    const output = getItem.execute(inexistentId);
+test("Should return an Item empty list", async () => {
+  const getItems = new GetItems(repositoryFactory)
+  const output = await getItems.execute()
 
-    expect(output).rejects.toThrow();
-});
+  expect(output).toHaveLength(0)
+})
