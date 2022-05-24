3b7056b884d3d262d720d1644ef2d3eb2c15bff2
Matheus Ricardo Brunelli <matheus.brunelli@gmail.com>
2022-05-24 22:31:09 +0000

    chore: indent code

--- test/integration/GetItems.test.ts
+++ test/integration/GetItems.test.ts
@@ -25,29 +25,29 @@ afterEach(async () => {
 });
 
 test("Should get all Items", async () => {
-  const registerItem = new RegisterItem(repositoryFactory, objectIdGenerator);
+    const registerItem = new RegisterItem(repositoryFactory, objectIdGenerator);
 
-  Array.from(Array(10)).forEach(async () => {
-      const input = {
-          description: "Ribeirão Lager",
-          price: 8.9,
-          abv: 5.2,
-          size: 600,
-          ingredients: new Ingredients("água", "malte", "lúpulo", "laranja"),
-      };
-      await registerItem.execute(input);
-  });
+    Array.from(Array(10)).forEach(async () => {
+        const input = {
+            description: "Ribeirão Lager",
+            price: 8.9,
+            abv: 5.2,
+            size: 600,
+            ingredients: new Ingredients("água", "malte", "lúpulo", "laranja"),
+        };
+        await registerItem.execute(input);
+    });
 
-  const getItems = new GetItems(repositoryFactory);
-  const output = await getItems.execute();
+    const getItems = new GetItems(repositoryFactory);
+    const output = await getItems.execute();
 
-  expect(output).toBeDefined();
-  expect(output).toHaveLength(10);
+    expect(output).toBeDefined();
+    expect(output).toHaveLength(10);
 });
 
 test("Should return an Item empty list", async () => {
-  const getItems = new GetItems(repositoryFactory)
-  const output = await getItems.execute()
+    const getItems = new GetItems(repositoryFactory);
+    const output = await getItems.execute();
 
-  expect(output).toHaveLength(0)
-})
+    expect(output).toHaveLength(0);
+});
