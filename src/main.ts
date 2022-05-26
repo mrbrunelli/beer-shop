b3d16744a11a89095816c3085df358192bdb1a01
mrbrunelli <matheus.brunelli@gmail.com>
2022-05-26 20:45:26 -0300

    refactor: change Database instance

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import MongoDBConnectionAdapter from "./Infra/Database/MongoDBConnectionAdapter";
+import DatabaseRepositoryFactory from "./Infra/Factory/DatabaseRepositoryFactory";
 import MemoryRepositoryFactory from "./Infra/Factory/MemoryRepositoryFactory";
 import ExpressAdapter from "./Infra/Http/ExpressAdapter";
 import Router from "./Infra/Http/Router";
 
-const repositoryFactory = new MemoryRepositoryFactory();
+const connection = new MongoDBConnectionAdapter();
+const repositoryFactory = new DatabaseRepositoryFactory(connection);
 const http = new ExpressAdapter();
 const router = new Router(http, repositoryFactory);
 router.init();
